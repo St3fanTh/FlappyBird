6323f0d880401666e36ba5111996d6ca494b25de
Stivi <stivi@example.com>
2026-04-22 21:05:24 +0200

    Add dynamic difficulty scaling to game

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,10 @@
 use wasm_bindgen::prelude::*;
 use web_sys::{CanvasRenderingContext2d, HtmlCanvasElement};
 
+const GRAVITY: f64 = 0.25;
+const JUMP_VELOCITY: f64 = -6.0;
+const BASE_PIPE_SPEED: f64 = 2.0;
+
 #[wasm_bindgen]
 pub fn init_panic_hook() {
     console_error_panic_hook::set_once();
@@ -58,31 +62,36 @@ impl Game {
             return;
         }
 
-        self.bird_velocity += 0.5;
+        self.bird_velocity += GRAVITY;
         self.bird_y += self.bird_velocity;
 
-        if self.frame_count % 100 == 0 {
+        let difficulty_factor = (self.score / 5).min(3) as f64;
+        let pipe_speed = BASE_PIPE_SPEED + difficulty_factor * 0.5;
+        let gap_size = 150.0 - (difficulty_factor * 10.0).min(30.0);
+
+        if self.frame_count % (100 - (difficulty_factor * 5.0) as i32).max(70) == 0 {
             self.pipes.push(Pipe {
                 x: self.width,
-                gap_top: 100.0 + (js_sys::Math::random() * 150.0),
-                gap_size: 150.0,
+                gap_top: 80.0 + (js_sys::Math::random() * 120.0),
+                gap_size,
                 scored: false,
             });
         }
 
         for pipe in &mut self.pipes {
-            pipe.x -= 3.0;
+            pipe.x -= pipe_speed;
         }
 
         self.pipes.retain(|p| p.x > -50.0);
 
         for pipe in &mut self.pipes {
-            if pipe.x < 60.0 && pipe.x + 40.0 > 20.0 {
+            let pipe_right = pipe.x + 40.0;
+            if pipe_right > 10.0 && pipe.x < 60.0 {
                 if self.bird_y < pipe.gap_top || self.bird_y + 30.0 > pipe.gap_top + pipe.gap_size {
                     self.game_over = true;
                 }
             }
-            if pipe.x + 40.0 < 20.0 && !pipe.scored {
+            if pipe_right < 10.0 && !pipe.scored {
                 self.score += 1;
                 pipe.scored = true;
             }
@@ -139,7 +148,7 @@ impl Game {
 
     pub fn jump(&mut self) {
         if !self.game_over {
-            self.bird_velocity = -8.0;
+            self.bird_velocity = JUMP_VELOCITY;
         }
     }
 
